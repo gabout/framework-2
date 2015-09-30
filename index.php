e9186b12d8a408582bb4acea9a697b3f254b14fb
Cristian Bernal <crisbera@gmail.com>
2015-09-30 18:27:39 -0500

    Modificación del archivo index principal

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-echo hola mundo
-?>
+/**
+ * Archivo index.php
+ * Punto de acceso principal a la aplicación
+ * 
+ * @author Carlos Abner <abnercgomez@gmail.com>
+ * @version 1.0 primera versión estable
+ * 
+ */
